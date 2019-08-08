33b99328dfcfda1b7d1e36dadb3993c091380892
yinghaoawang <yinghao.a.wang@gmail.com>
2019-08-07 23:53:10 -0700

    register callbacks on connect

--- src/client/networking.js
+++ src/client/networking.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 const Constants = require('../shared/constants');
-const socket = io(`ws://${window.location.host}`);
+const socket = io(`ws://${window.location.host}`, { reconnection: false });
 
 const connectedPromise = new Promise(resolve => {
     socket.on('connect', () => {
@@ -12,6 +12,12 @@ const connectedPromise = new Promise(resolve => {
 
 export const connect = (onGameOver) => {
     return connectedPromise.then(() => {
+        // register callbacks
+        //socket.on(Constants.MSG_TYPES.GAME_UPDATE, processGameUpdate);
+        //socket.on(Constants.MSG_TYPES.GAME_OVER, onGameOver);
+        socket.on('disconnect', () => {
+            console.log('Disconnected from server.');
+        });
     });
 };
 
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,3 +1,9 @@
 module.exports = {
     pingas: 'pingas',
+    MSG_TYPES: {
+        JOIN_GAME: 'join_game',
+        GAME_UPDATE: 'update',
+        INPUT: 'input',
+        GAME_OVER: 'dead',
+    }
 };
